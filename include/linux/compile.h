fdb1fb2b032cc088ee83bb33ce0737aca7fc98df
imoseyon <imoseyon@gmail.com>
2011-07-28 21:37:43 -0700

    cleanup

--- include/linux/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 1 */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 Thu Jul 28 16:03:04 PDT 2011"
-#define LINUX_COMPILE_TIME "16:03:04"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILE_DOMAIN "corp.meebo.com"
-#define LINUX_COMPILER "gcc version 4.4.5 (Ubuntu/Linaro 4.4.4-14ubuntu4) "
